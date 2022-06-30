497cf6e671f1bb8ca39321c9c119790447419222
MurayamaKazuya <kamiyu.kp@gmail.com>
2022-06-30 14:44:53 +0900

    Add:top作成分

---
+++ top/JS/script.js
@@ -0,0 +1,17 @@
+$(function () {
+  $('.mainvisual').slick({
+    arrows: false,
+    slideToShow: 1,
+    dots: true,
+    fade: true,
+    autoplaySpeed: 3000
+  });
+
+  $('.service-visual').slick({
+    arrows: true,
+    slideToShow: 1,
+    dots: true,
+    fade: true,
+    autoplaySpeed: 3000
+  });
+});
