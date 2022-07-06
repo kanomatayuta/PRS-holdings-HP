9da095ceeb5f11f3cb30a10230d2f45fd93d397a
鹿又勇太 <>
2022-07-06 14:17:13 +0900

    Add:サービス-4にレスポンシブ機能を追加(SES事業) hamburgerNaviを追加  media queriesを追加 ・max-width:960px時（tb用） ・max-width:520px時（mb用） ・max-width:320px時（それ以外）
    ※パンクズリストのレイアウト調整（未）
    内容：レスポンシブ時リストのレイアウトが崩れる。

--- servic-4/js/sample.js
+++ servic-4/js/sample.js
@@ -7,3 +7,26 @@ for (let i = 0; i < value.length; i++)
   // valueクラスの親要素に取得しwitdthを指定
   value[i].parentNode.style.width = text;
 }
+
+/*----------------------------
+ * hamburger-navの実装 SP版
+ *----------------------------*/
+const hamburgerBtn = document.getElementById('js-hamburger');
+const drawerNav = document.getElementById('js-drawer');
+let flag = false;
+
+hamburgerBtn.addEventListener('click', function () {
+  if (!flag)
+  {
+    drawerNav.classList.add('drawer--active');
+    hamburgerBtn.classList.add('btn--active');
+    flag = true;
+  } else
+  {
+    drawerNav.classList.remove('drawer--active');
+    hamburgerBtn.classList.remove('btn--active');
+    flag = false;
+  }
+});
+
+
