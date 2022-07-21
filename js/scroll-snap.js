8237b62809b0d4ff6cfe31402beb50f2b370c68c
MurayamaKazuya <kamiyu.kp@gmail.com>
2022-07-21 20:18:15 +0900

    Fix：スクロールスピードを1000→700へ変更

--- js/scroll-snap.js
+++ js/scroll-snap.js
@@ -2,14 +2,14 @@ var current;
 $.scrollify({
   section: ".split",
   interstitialSection: "header,footer,.global-contact",
+  scrollSpeed: 700,
   setHeights: false,
   before: function (i, split) {
     current = i;
   },
 });
 $(window).on('resize', function () {
-  if (current)
-  {
+  if (current) {
     var currentScrl = $('.split').eq(current).offset().top;
     $(window).scrollTop(currentScrl);
   }
