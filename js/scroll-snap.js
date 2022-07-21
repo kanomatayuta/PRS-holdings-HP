3ab28398f82f04cb3e87c1315936d81657bdfcaa
鹿又勇太 <>
2022-07-21 20:38:36 +0900

    Fix:  sectionごとのmax-widthを廃止

--- js/scroll-snap.js
+++ js/scroll-snap.js
@@ -1,17 +1,18 @@
-var current;
-$.scrollify({
-  section: ".split",
-  interstitialSection: "header,footer,.global-contact",
-  scrollSpeed: 300,
-  easing: "swing",
-  setHeights: false,
-  before: function (i, split) {
-    current = i;
-  },
-});
-$(window).on('resize', function () {
-  if (current) {
-    var currentScrl = $('.split').eq(current).offset().top;
-    $(window).scrollTop(currentScrl);
-  }
-});
+// var current;
+// $.scrollify({
+//   section: ".split",
+//   interstitialSection: "header,footer,.global-contact",
+//   scrollSpeed: 1000,
+//   easing: "swing",
+//   setHeights: false,
+//   before: function (i, split) {
+//     current = i;
+//   },
+// });
+// $(window).on('resize', function () {
+//   if (current)
+//   {
+//     var currentScrl = $('.split').eq(current).offset().top;
+//     $(window).scrollTop(currentScrl);
+//   }
+// });
